966e795d5167c135e6cb749880facc86128d0ee8
Baral Alan <abaral@andreani.com>
2020-12-21 19:21:34 -0300

    cambios base endpoint de SLU

---
+++ CLU/Controllers/SolicitudLegitimoUsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using CLU.Datos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CLU.Controllers
+{
+    public class SolicitudLegitimoUsuarioController : ApiController
+    {
+        modelEntities BD = new modelEntities();
+        public IEnumerable<SolicitudLegitimoUsuario> Get() 
+        {
+            var listado = BD.SolicitudLegitimoUsuario.ToList();
+            return listado;
+        } 
+
+    }
+}
